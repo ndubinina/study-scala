47501b451ba74b586c9def6829ffab7b79b59843
ndubinina <ndubinina@neoflex.ru>
2020-03-13 11:06:50 +0300

    екоторые задачи

--- src/main/scala/nina/s99/ListTasks.scala
+++ src/main/scala/nina/s99/ListTasks.scala
@@ -12,4 +12,34 @@ object ListTasks extends App {
     case _ :: tail => penultimateRec(list.tail)
     case _ => throw new NoSuchElementException
   }
+
+  def get[T](i: Int, list: List[T]): T = (list, i) match {
+    case (_, _) if (i < 0 || i >= list.size) => throw new ArrayIndexOutOfBoundsException
+    case (h :: tail, 0) => h
+    case (h :: tail, _) => get(i-1, tail)
+  }
+
+  def length[T](list: List[T]): Int = list match {
+    case h :: tail => length(tail) + 1
+    case Nil => 0
+  }
+
+  def reverse[T](list: List[T]): List[T] = list match {
+    case Nil => Nil
+    case h :: tail => reverse(tail) :+ h
+    case _ => list
+  }
+
+  def isPalindrome[T](list: List[T]): Boolean = {
+    if (list == Nil) false else list == list.reverse
+  }
+
+  def flatten[T](lists: List[T]*): List[T] = lists.flatten.toList
+
+  def compress[T](list: List[T]):List[T] = list match {
+    case a :: b :: tail => if (a != b) (a +: compress(b +: tail)) else compress(b +: tail)
+    case Nil => Nil
+    case _ => list
+  }
+
 }
--- src/test/scala/nina/s99/ListTasksTest.scala
+++ src/test/scala/nina/s99/ListTasksTest.scala
@@ -1,7 +1,5 @@
 package nina.s99
 
-import java.util.NoSuchElementException
-
 import org.scalatest.FunSuite
 
 class ListTasksTest extends FunSuite {
@@ -19,5 +17,48 @@ class ListTasksTest extends FunSuite {
       ListTasks.penultimateRec(List(Nil))
     }
     assert(ListTasks.penultimateRec(List("one", "two")) == "one")
+  }
+
+  test("ListTasks.get") {
+    assertThrows[ArrayIndexOutOfBoundsException]{
+      ListTasks.get(2, List(Nil))
+    }
+    assertThrows[ArrayIndexOutOfBoundsException]{
+      ListTasks.get(2, List(0, 1))
+    }
+    assert(ListTasks.get(3, List(10, 11, 12, 13)) === 13)
+    assert(ListTasks.get(0, List(10, 11, 12, 13)) === 10)
+  }
+
+  test("ListTasks.length") {
+    assert(ListTasks.length(List(1, 2, 5, 4)) === 4)
+    assert(ListTasks.length(Nil) === 0)
+  }
+
+  test("ListTasks.reverse") {
+    assert(ListTasks.reverse(List(5)) == List(5))
+    assert(ListTasks.reverse(Nil) == Nil)
+    assert(ListTasks.reverse(List(1,2,3)) == List(3,2,1))
+  }
+
+  test("ListTasks.isPalindrome") {
+    assert(ListTasks.isPalindrome(Nil) === false)
+    assert(ListTasks.isPalindrome(List(1)) === true)
+    assert(ListTasks.isPalindrome(List(1,2,2)) === false)
+    assert(ListTasks.isPalindrome(List(1,2,2,1)) === true)
+    assert(ListTasks.isPalindrome(List(1,2,1)) === true)
+  }
+
+  test("ListTasks.flatten") {
+    assert(ListTasks.flatten(Nil, List(0, 3), List(2, 5, 6)) === List(0,3,2,5,6))
+    assert(ListTasks.flatten(List(7)) === List(7))
+    assert(ListTasks.flatten(Nil) === Nil)
+  }
+
+  test("ListTasks.compress") {
+    assert(ListTasks.compress(List(1,1,1,2,1,1,3,3,3,4,5)) === List(1,2,1,3,4,5))
+    assert(ListTasks.compress(Nil) === Nil)
+    assert(ListTasks.compress(List(1)) === List(1))
+    assert(ListTasks.compress(List(1, 1)) === List(1))
   }
 }
